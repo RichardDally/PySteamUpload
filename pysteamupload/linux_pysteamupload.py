1b99705338d28610d0f5f6b180507bc033599a6b
TrellixVulnTeam <charles.mcfarland@trellix.com>
2022-12-09 03:51:39 +0000

    Adding tarfile member sanitization to extractall()

--- pysteamupload/linux_pysteamupload.py
+++ pysteamupload/linux_pysteamupload.py
@@ -15,4 +15,26 @@ class LinuxPySteamUpload(GenericPySteamUpload):
     @abstractmethod
     def extract_steamcmd_archive(self) -> None:
         with tarfile.open(self.get_archive_path(), 'r:gz') as f:
-            f.extractall(path=self.root_directory)
+            
+            import os
+            
+            def is_within_directory(directory, target):
+                
+                abs_directory = os.path.abspath(directory)
+                abs_target = os.path.abspath(target)
+            
+                prefix = os.path.commonprefix([abs_directory, abs_target])
+                
+                return prefix == abs_directory
+            
+            def safe_extract(tar, path=".", members=None, *, numeric_owner=False):
+            
+                for member in tar.getmembers():
+                    member_path = os.path.join(path, member.name)
+                    if not is_within_directory(path, member_path):
+                        raise Exception("Attempted Path Traversal in Tar File")
+            
+                tar.extractall(path, members, numeric_owner=numeric_owner) 
+                
+            
+            safe_extract(f, path=self.root_directory)

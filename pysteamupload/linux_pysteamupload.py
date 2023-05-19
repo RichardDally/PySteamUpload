94f9234535cf0af9a700bf0767cdc07978f0dfac
Richard Dally <r.dally@protonmail.com>
2023-05-19 14:19:27 +0200

    Tidy up security patch

--- pysteamupload/linux_pysteamupload.py
+++ pysteamupload/linux_pysteamupload.py
@@ -1,3 +1,4 @@
+import os
 import tarfile
 from abc import abstractmethod
 from pysteamupload.generic_pysteamupload import GenericPySteamUpload
@@ -15,26 +16,19 @@ class LinuxPySteamUpload(GenericPySteamUpload):
     @abstractmethod
     def extract_steamcmd_archive(self) -> None:
         with tarfile.open(self.get_archive_path(), 'r:gz') as f:
-            
-            import os
-            
-            def is_within_directory(directory, target):
-                
-                abs_directory = os.path.abspath(directory)
-                abs_target = os.path.abspath(target)
-            
-                prefix = os.path.commonprefix([abs_directory, abs_target])
-                
-                return prefix == abs_directory
-            
-            def safe_extract(tar, path=".", members=None, *, numeric_owner=False):
-            
-                for member in tar.getmembers():
-                    member_path = os.path.join(path, member.name)
-                    if not is_within_directory(path, member_path):
-                        raise Exception("Attempted Path Traversal in Tar File")
-            
-                tar.extractall(path, members, numeric_owner=numeric_owner) 
-                
-            
-            safe_extract(f, path=self.root_directory)
+            self._safe_extract(tar_file_handler=f, path=str(self.root_directory))
+
+    @staticmethod
+    def _is_within_directory(directory: str, target: str) -> bool:
+        abs_directory = os.path.abspath(directory)
+        abs_target = os.path.abspath(target)
+        prefix = os.path.commonprefix([abs_directory, abs_target])
+        return prefix == abs_directory
+
+    @staticmethod
+    def _safe_extract(tar_file_handler, path: str, members=None, *, numeric_owner=False) -> None:
+        for member in tar_file_handler.getmembers():
+            member_path = os.path.join(path, member.name)
+            if not LinuxPySteamUpload._is_within_directory(path, member_path):
+                raise RuntimeError("Attempted Path Traversal in Tar File")
+        tar_file_handler.extractall(path, members, numeric_owner=numeric_owner)

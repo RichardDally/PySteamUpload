85d24820e73d7c189675d37bb839153c0b47f2b1
Richard Dally <r.dally@protonmail.com>
2023-05-19 14:06:42 +0200

    Set version to 0.2.0

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ with open("requirements.txt") as f:
 setuptools.setup(
     author="Richard Dally",
     name="pysteamupload",
-    version="0.1.9",
+    version="0.2.0",
     description="SteamCMD leveraged by CPython to upload binaries",
     url="https://github.com/RichardDally/PySteamUpload",
     license="GNU Lesser General Public License v3.0",
